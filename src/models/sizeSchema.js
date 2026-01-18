f62ea7543008f70b8ee91d38c5dc39e9ef8a9aad
kpmisthah <kpmisthah6@gmail.com>
2026-01-18 23:28:01 +0530

    style: format size schema code

--- src/models/sizeSchema.js
+++ src/models/sizeSchema.js
@@ -1,20 +1,20 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const sizeSchema = new Schema({
-    
+
     product: {
-          type: Schema.Types.ObjectId,
-          ref: "Product", // Links to the Product model
-          required: true,
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
     },
-    size:{
-        type:String,
-        required:true
+    size: {
+        type: String,
+        required: true
     },
-    quantity:{
-        type:Number,
-        required:true
+    quantity: {
+        type: Number,
+        required: true
     }
 })
 
-export const Size = mongoose.model('Size',sizeSchema)
+export const Size = mongoose.model('Size', sizeSchema)
