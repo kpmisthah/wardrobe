f9e3f5821e840c1dbdd1c560e85477f4a1a17dad
kpmisthah <kpmisthah6@gmail.com>
2026-01-18 23:28:03 +0530

    style: format size.js code

--- public/javascript/size.js
+++ public/javascript/size.js
@@ -1,29 +1,30 @@
-  
+
 const form = document.getElementById('size-product')
-form.addEventListener('submit',async(event)=>{
+form.addEventListener('submit', async (event) => {
     event.preventDefault()
 
-const product = document.getElementById('product').value
-const size =  document.getElementById('size').value 
-const quantity = document.getElementById('quantity').value
-    try{
-    const response = await fetch('/admin/add-size',{
-    method:'POST',
-    headers:{'Content-Type':'application/json'},
-    body:JSON.stringify({product,size,quantity})
-})
+    const product = document.getElementById('product').value
+    const size = document.getElementById('size').value
+    const quantity = document.getElementById('quantity').value
+    try {
+        const response = await fetch('/admin/add-size', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ product, size, quantity })
+        })
 
-if(response.ok){
-    Swal.fire({
-        title: 'Success!',
-        text: 'Size has been added successfully.',
-        icon: 'success',
-        confirmButtonText: 'OK'
-    }).then(() => {
-        location.href = '/admin/size';
-    });
-}
-} catch (error) {
-console.log("The error is "+error)
-}
+        if (response.ok) {
+            Swal.fire({
+                title: 'Success!',
+                text: 'Size has been added successfully.',
+                icon: 'success',
+                confirmButtonText: 'OK'
+            }).then(() => {
+                // location.href = '/admin/size';
+                document.getElementById('size-product').reset(); // Clear the form
+            });
+        }
+    } catch (error) {
+        console.log("The error is " + error)
+    }
 })
