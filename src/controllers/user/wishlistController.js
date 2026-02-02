e4c8bba6f51dc43132bd6e6d9ae8079ada043912
kpmisthah <kpmisthah6@gmail.com>
2026-02-03 02:12:45 +0530

    Refactor wishlist controller: add stock validation and optimize queries

--- src/controllers/user/wishlistController.js
+++ src/controllers/user/wishlistController.js
@@ -2,85 +2,101 @@ import { Cart } from "../../models/cartSchema.js";
 import { Product } from "../../models/productSchema.js";
 import { User } from "../../models/userSchema.js";
 import { Wishlist } from "../../models/wishlistSchema.js";
+import { Size } from "../../models/sizeSchema.js";
 
 
-const loadWishlist = async(req,res)=>{
+const loadWishlist = async (req, res) => {
     try {
         const user = req.session.user
-        const wishlist = await Wishlist.findOne({userId:user}).populate('items.product')
-       if(user){
-        let userData = await User.findOne({_id:user})
-        return res.render('user/wishlist',{wishlist,user:userData})
-       }else{
-        return res.redirect('/login')
-       }
-        
-        
+        // Use lean() to get a plain JavaScript object that we can modify
+        const wishlist = await Wishlist.findOne({ userId: user }).populate('items.product').lean();
+
+        if (user) {
+            let userData = await User.findOne({ _id: user });
+
+            // Enrich wishlist items with stock information
+            if (wishlist && wishlist.items && wishlist.items.length > 0) {
+                for (const item of wishlist.items) {
+                    if (item.product && item.size) {
+                        const sizeDoc = await Size.findOne({
+                            product: item.product._id,
+                            size: item.size
+                        });
+                        // Attach stock quantity to the item for the view to use
+                        item.stockQuantity = sizeDoc ? sizeDoc.quantity : 0;
+                    } else {
+                        item.stockQuantity = 0;
+                    }
+                }
+            }
+
+            return res.render('user/wishlist', { wishlist, user: userData })
+        } else {
+            return res.redirect('/login')
+        }
+
+
     } catch (error) {
-        
+        console.log("Error in loadWishlist:", error);
+        res.status(500).send("Internal Server Error");
     }
 }
 
-const addToWishlist = async(req,res)=>{
+const addToWishlist = async (req, res) => {
     try {
         const user = req.session.user
-        const{productId,size,quantity} = req.body
-        let wishlist = await Wishlist.findOne({userId:user})
-        if(!wishlist){
-            wishlist = new Wishlist({userId:user,items:[]})
+        const { productId, size, quantity } = req.body
+        let wishlist = await Wishlist.findOne({ userId: user })
+        if (!wishlist) {
+            wishlist = new Wishlist({ userId: user, items: [] })
         }
-        const items = wishlist.items.some((item)=>item.product == productId && item.size == size && item.quantity == 1)
+        const items = wishlist.items.some((item) => item.product == productId && item.size == size && item.quantity == 1)
         if (quantity > 1) {
             return res.status(400).json({
                 message: "Only one quantity of a product can be added to the wishlist"
             });
         }
-        if(!items){
+        if (!items) {
             wishlist.items.push({
-                product:productId,
+                product: productId,
                 size,
                 quantity
-            })     
-        }else{
-            return res.status(400).json({message:"product is already exist"})
+            })
+        } else {
+            return res.status(400).json({ message: "product is already exist" })
         }
         await wishlist.save()
-        return res.status(200).json({message:"product addedd to wishlist"})
+        return res.status(200).json({ message: "product addedd to wishlist" })
     } catch (error) {
-        console.log("The error is "+error)
+        console.log("The error is " + error)
     }
 }
 
-const wishlistToCart = async(req,res)=>{
+const wishlistToCart = async (req, res) => {
     try {
         const userId = req.session.user
-        console.log(userId);
-        
-        const{productId,productName,productSize,productPrice} = req.body
-        console.log("prodcut id +++"+productId);
-        console.log("prodcut name==="+productName);
-        console.log("prodcut size----"+productSize);
-        console.log("price==================="+productPrice);
-        
-        console.log();
-        
-        
-        
-        
-        
-        const cart = await Cart.findOne({userId})
-        console.log("The wish cart"+cart);
-        
-        const wishlist = await Wishlist.findOne({userId})
-        console.log("wish"+wishlist);
-        
-        if(!cart){
-            cart = new Cart({userId,items:[],bill:0})
+        const { productId, productName, productSize, productPrice } = req.body
+
+        // Check stock availability first
+        const sizeDoc = await Size.findOne({ product: productId, size: productSize });
+        if (!sizeDoc || sizeDoc.quantity < 1) {
+            return res.status(400).json({ message: "Product is out of stock" });
         }
-        const cartIndex =  cart.items.findIndex((item)=>item.product.toString()==productId && item.size == productSize)
-        if(cartIndex != -1){
-            return res.status(400).json({message:"product is already in cart"})
-        }else{
+
+        let cart = await Cart.findOne({ userId })
+
+        if (!cart) {
+            cart = new Cart({ userId, items: [], bill: 0 })
+        }
+
+        const cartIndex = cart.items.findIndex((item) => item.product.toString() == productId && item.size == productSize)
+
+        if (cartIndex != -1) {
+            // Check if adding 1 more would exceed stock? 
+            // Usually cart add simply adds, but here we adding from wishlist (qty 1)
+            // If already in cart, we return 'already in cart' message as per generic logic
+            return res.status(400).json({ message: "product is already in cart" })
+        } else {
             const newItem = {
                 product: productId,
                 name: productName,
@@ -91,31 +107,31 @@ const wishlistToCart = async(req,res)=>{
             };
             cart.items.push(newItem)
         }
+
         cart.bill += Number(productPrice);
-        console.log("The bill si"+cart.bill)
         await cart.save()
-        return res.status(200).json({message:"Product is added to cart successfully"})
+        return res.status(200).json({ message: "Product is added to cart successfully" })
     } catch (error) {
-        console.log("erorr is "+error);
-        
+        console.log("error is " + error);
+        res.status(500).json({ message: "Internal server error" });
     }
 }
 
-const removeWishlist = async(req,res)=>{
+const removeWishlist = async (req, res) => {
     try {
         const userId = req.session.user
-        const {productId} = req.params
-        console.log("The aprams is"+productId)
-        const wishlist = await Wishlist.findOneAndUpdate({userId},{$pull:{items:{_id:productId}}})
-        console.log("The wishlsit is"+wishlist);
-        
-        if(!wishlist){
-            return res.status(404).json({message:"wishlist not found"})
+        const { productId } = req.params
+        console.log("The aprams is" + productId)
+        const wishlist = await Wishlist.findOneAndUpdate({ userId }, { $pull: { items: { _id: productId } } })
+        console.log("The wishlsit is" + wishlist);
+
+        if (!wishlist) {
+            return res.status(404).json({ message: "wishlist not found" })
         }
-        return res.status(200).json({message:"wishlist deleted successfully"})
-       
+        return res.status(200).json({ message: "wishlist deleted successfully" })
+
     } catch (error) {
-        
+
     }
 }
-export{loadWishlist,addToWishlist,wishlistToCart,removeWishlist}
+export { loadWishlist, addToWishlist, wishlistToCart, removeWishlist }
